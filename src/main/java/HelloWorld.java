f4ff699f7482e911548c84d7f677a80f9315c988
Sandhya Narayani Ambadevi Kaltenegger <fhb241910@fh-vie.ac.at>
2026-03-27 14:34:06 +0100

    Test: read the file line by line to verify that the application can access and read the document.

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,2 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class HelloWorld {
+    public static void main(String[] args) {
+
+        File hotels= new File("src/main/resources/hotels.txt");
+        Scanner scanner1 = null;
+        try {
+            scanner1 = new Scanner(hotels);
+            while (scanner1.hasNextLine()) {
+                System.out.println(scanner1.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        File occupancies= new File("src/main/resources/occupancies.txt");
+        Scanner scanner2 = null;
+        try {
+            scanner2 = new Scanner(occupancies);
+            while (scanner2.hasNextLine()) {
+                System.out.println(scanner2.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
