cff0d6fc705c4d7c43986fd70c3a27ca5a197000
Sandhya Narayani Ambadevi Kaltenegger <fhb241910@fh-vie.ac.at>
2026-05-02 12:08:40 +0200

    Logo adapted

--- src/main/java/LogoComponent.java
+++ src/main/java/LogoComponent.java
@@ -1,29 +1,28 @@
-//import javax.imageio.ImageIO;
-//import javax.swing.*;
-//import java.awt.*;
-//import java.awt.image.BufferedImage;
-//import java.io.File;
-//import java.io.IOException;
-//
-//public class LogoComponent extends JLabel {
-//
-//    //TO DO: kopiere das hier hinein in dein window
-//    //mainPanel.add(new LogoComponent(),  BorderLayout.NORTH);
-//
-//    public LogoComponent() {
-//        BufferedImage img = null;
-//
-//        try {
-//            img = ImageIO.read(new File("src/main/resources/Logo.jpg"));
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//            getSca
-//        }
-//        Image scaled = img.getScaledInstance(150, 40, Image.SCALE_SMOOTH);
-//        setIcon(new ImageIcon(scaled));
-//    }
-//}
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class LogoComponent extends JLabel {
+
+    //TO DO: kopiere das hier hinein in dein window
+    //mainPanel.add(new LogoComponent(),  BorderLayout.NORTH);
+
+    public LogoComponent() {
+        BufferedImage img = null;
+
+        try {
+            img = ImageIO.read(new File("src/main/resources/Logo.jpg"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Image scaled = img.getScaledInstance(-1, 80, Image.SCALE_SMOOTH);
+        setIcon(new ImageIcon(scaled));
+    }
+}
+/*import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -69,4 +68,4 @@ public class LogoComponent extends JComponent {
         }
         return new Dimension(250, 80);
     }
-}
+}*/
