c808e5e48498cf0d31b5dc38302e543d86838f13
sirBabushka <yuanoops@gmail.com>
2026-05-04 06:10:47 +0200

    US ohne Jframes

--- src/main/java/db/DatabaseConnection.java
+++ src/main/java/db/DatabaseConnection.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    // TODO:
     private static final String URL = "jdbc:sqlserver://185.119.119.126:1433;database=DanubeDevs;encrypt=true;trustServerCertificate=true";
     private static final String USER = "danube";
     private static final String PASSWORD = "danube";
