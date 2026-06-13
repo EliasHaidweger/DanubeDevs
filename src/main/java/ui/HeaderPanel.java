eb7578d22a92b636d902ba9a947b1330e829ddec
Sandhya Narayani Ambadevi Kaltenegger <fhb241910@fh-vie.ac.at>
2026-06-13 20:34:59 +0200

    US #17: Logo on Panel

---
+++ src/main/java/ui/HeaderPanel.java
@@ -0,0 +1,22 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Displays the NOE logo at the top of the window (US 17).
+ */
+public class HeaderPanel extends JPanel {
+
+    public HeaderPanel() {
+        setLayout(new FlowLayout(FlowLayout.LEFT, 10, 5));
+        setBackground(Color.WHITE);
+
+        ImageIcon icon = LogoHelper.getLogoIcon(70);
+        if (icon != null) {
+            add(new JLabel(icon));
+        } else {
+            add(new JLabel("Lower Austria Tourist Portal"));
+        }
+    }
+}
